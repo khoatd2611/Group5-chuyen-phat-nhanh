8683fc1edf57aa5569dd53df2164b6e775cda985
Tuan Hung <hung.nt2076@aptechlearning.edu.vn>
2023-05-04 22:11:37 +0700

    test hehehe

---
+++ src/deliverymanagement123/db.java
@@ -0,0 +1,24 @@
+///*
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+// * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+// */
+//package deliverymanagement123;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//
+///**
+// *
+// * @author tuan1
+// */
+//public class db {
+//    public static  Connection mycon(){
+//        Connection con = null;
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//            con = DriverManage.getConnection("jdbc:mysql://localhost/delivery_management","root","");
+//            return con;
+//        } catch (Exception e) {
+//        }
+//    }
+//}
